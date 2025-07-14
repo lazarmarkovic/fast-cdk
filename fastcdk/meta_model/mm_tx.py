a29bd95183d542a57f0e182bfaec2d828eb0ba52
Lazar Markovic <office@lazarmarkovic.net>
2025-07-14 19:43:23 +0200

    Restructure and redefine

--- fastcdk/meta_model/mm_tx.py
+++ fastcdk/meta_model/mm_tx.py
@@ -3,3 +3,10 @@ VPCDef: 'VPC' statements*=AssignmentStatement;
 AssignmentStatement: var=ID '=' value=Value;
 Value: INT;
 """
+
+M2 = """
+VPCDef: 'VPC' statements*=AssignmentStatement;
+AssignmentStatement: var=ID '=' value=Value;
+Value: INT | STRING;
+STRING: '"' value=ID '"';
+"""
