70804ad1bd11ec7c844483074205774f3d7d7487
Lazar Markovic <office@lazarmarkovic.net>
2025-11-22 23:30:26 +0100

    Stuff

--- fastcdk/language.py
+++ fastcdk/language.py
@@ -8,6 +8,15 @@ from os.path import dirname, join
 
 @language("fastcdk", "*.fcdk")
 def fastcdk_language():
+  metamodel = MetaModel().mm
+  sp = SemanticProcessors()
+  metamodel.register_obj_processors(sp.obj_processors)
+
+  return metamodel_from_file(join(dirname(__file__), "dsl" , "grammar.tx"))
+
+
+@language("fastcdk_def", "*.fcdk_def")
+def fastcdk_def_language():
   # metamodel = MetaModel().mm
   # sp = SemanticProcessors()
   # metamodel.register_obj_processors(sp.obj_processors)
